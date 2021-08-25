37e0b79729eca6b60c466a6f3a06a425586ec699
AlexWilsonNC <alexwilson0017@yahoo.com>
2021-08-25 19:50:54 -0400

    onclick listeners for blue save buttons and time tracker

--- script.js
+++ script.js
@@ -3,4 +3,21 @@
 var today = moment();
 
 var currentDayHeader = today.format('dddd, MMMM Do');
-$("#currentDay").text(currentDayHeader); // using jQuery to avoid typing var _ = document.getElementById('#currentDay');
+$('#currentDay').text(currentDayHeader); // using jQuery to avoid typing var _ = document.getElementById('#currentDay');
+
+$(document).ready(function() {
+    var text = $(this).siblings('.description').val();
+    var time = $(this).parent().attr('id');
+
+    localStorage.setItem(time, text);
+})
+
+function timeOfDay() {
+    var currentTime = today.hour();
+
+    $('.time-block').each(function() {
+        var timeBlock = parseInt($(this).attr('id').split('hour')[1]);
+        if (timeBlock < currentTime) {
+            
+    }
+};
